bd0457ecebde0656c58e846acc74304f9eb99fa2
Inheritor-Vision <valentin.boulou@gmail.com>
2022-09-03 22:17:24 +0200

    Main: Better handling of Poxy feature

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ use serde_json::{Value};
 use reqwest::{Client, header};
 use futures::executor::block_on;
 
+#[cfg(feature = "proxy")]
+use std::fs::File;
+#[cfg(feature = "proxy")]
+use std::io::prelude::*;
+
 static APP_USER_AGENT: &str = concat!(
 	env!("CARGO_PKG_NAME"),
 	"/",
@@ -32,20 +37,32 @@ fn initialize_headers() -> header::HeaderMap{
 
 }
 
+#[cfg(feature = "proxy")]
+fn get_certificate() -> reqwest::Certificate{
+	let mut buff = Vec::new();
+	File::open("data/cacert.der").unwrap().read_to_end(&mut buff).unwrap();
+	let cert = reqwest::Certificate::from_der(&buff).unwrap();
+
+	cert
+}
+
 fn get_client(headers: header::HeaderMap) -> Client{
+	let client: reqwest::ClientBuilder;
 
-	let mut client = Client::builder()
-		.user_agent(APP_USER_AGENT)
-		.default_headers(headers);
-
-	client = if cfg!(feature = "proxy") {
-		client
+	#[cfg(not(feature = "proxy"))]{
+		client = Client::builder()
+			.user_agent(APP_USER_AGENT)
+			.default_headers(headers);
+	}
+	
+	#[cfg(feature = "proxy")]{
+		client = Client::builder()
+			.user_agent(APP_USER_AGENT)
+			.default_headers(headers)
 			.proxy(reqwest::Proxy::http("http://127.0.0.1:8080").unwrap())
 			.proxy(reqwest::Proxy::https("http://127.0.0.1:8080").unwrap())
-			.danger_accept_invalid_certs(true)
-	}else{
-		client
-	};
+			.add_root_certificate(get_certificate());
+	} 
 	
 	client.build().unwrap()
 
