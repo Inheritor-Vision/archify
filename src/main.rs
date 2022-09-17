3b921f0f866f53eaf77763b8c4939d7a917dbafa
Inheritor-Vision <valentin.boulou@gmail.com>
2022-09-17 22:56:47 +0200

    Main: CTRL Z going wrong. Undoing change

--- src/main.rs
+++ src/main.rs
@@ -158,7 +158,7 @@ pub async fn authenticate_user(mut client: tokio_postgres::Client, mut client_sp
 	};
 
 	// Update FullToken anyway because a new refresh token has been issued
-	set_full_token(&mut client, &user.user_id, &token);
+	set_full_token(&mut client, &user.user_id, &token).await;
 
 	user.cookie
 
